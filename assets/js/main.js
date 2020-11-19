fb0e548687b50d491b8a9a2dcf6bd2d4e66526a1
Filipe Ritto <filipe.ritto@gmail.com>
2020-11-19 15:08:55 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,36 @@ modalSample[i].addEventListener('click', modalSampleToggle);
 //------------------------ Modal Header
 
 new ClipboardJS('.btn-copy');
+
+//== Anchor Scrollsmooth
+// Vanilla JavaScript Scroll to Anchor @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+(function() {
+    
+    function scrollTo() {
+        const links = document.querySelectorAll('.cardslink');
+        links.forEach(each => (each.onclick = scrollAnchors));
+    }
+
+    function scrollAnchors(e, respond = null) {
+        const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+        e.preventDefault();
+        var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+        const targetAnchor = document.querySelector(targetID);
+        if (!targetAnchor) return;
+        const originalTop = distanceToTop(targetAnchor);
+        window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+        const checkIfDone = setInterval(function() {
+            const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+            if (distanceToTop(targetAnchor) === 0 || atBottom) {
+                targetAnchor.tabIndex = '-1';
+                targetAnchor.focus();
+                window.history.pushState('', '', targetID);
+                clearInterval(checkIfDone);
+            }
+        }, 150);
+    }
+
+	scrollTo();
+})();
+
+//======================== /Anchor Scrollsmooth
